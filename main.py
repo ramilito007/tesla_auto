f56f1d87cf5def9cd3f4710b1809e520f9a4d1f9
Gilula, Rami <Rami_Gilula@bmc.com>
2022-02-09 23:50:55 +0200

    start of tesla project - make login

---
+++ main.py
@@ -0,0 +1,35 @@
+import teslapy
+import teslapy
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from argparse import ArgumentParser
+
+chromedriver = 'C:\\chromedriver\\chromedriver.exe'
+
+
+def custom_auth(url):
+    driver = webdriver.Chrome(chromedriver)
+    driver.get(url)
+    WebDriverWait(driver, 300).until(EC.url_contains('void/callback'))
+    return driver.current_url
+    # driver.quit()
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-e", "--email", default="ramig23@gmail.com", help="Tesla Email Account")
+    args = parser.parse_args()
+
+    # tesla = teslapy.Tesla(args.email)
+    with teslapy.Tesla(args.email, authenticator=custom_auth) as tesla:
+        ret = tesla.fetch_token()
+    print(f"The returned token is: {ret}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
